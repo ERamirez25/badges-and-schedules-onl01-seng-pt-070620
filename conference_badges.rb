0863b1803aba58ab2f392a41b5d186eac406ae90
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-13 21:05:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,4 +7,6 @@ def batch_badge_creator(attendees)
   attendees =  ["Hello, my name is Edsger.", "Hello, my name is Ada.", "Hello, my name is Charles.", "Hello, my name is Alan.", "Hello, my name is Grace.", "Hello, my name is Linus.", "Hello, my name is Matz."]
 end
 
-
+def assign_rooms(speakers)
+  
+end 
