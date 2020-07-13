a8423cf6bd035164167063211010cfbc517e2872
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-13 21:18:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,5 +9,7 @@ end
 
 def assign_rooms(speakers)
   new_arr = []
-  speakers.each_with_index{|name,
+  speakers.each_with_index do |name, i|
+   new_arr << "Hello, #{name}! You'll ne assigned to room #{i+1}!"
 end 
+end
